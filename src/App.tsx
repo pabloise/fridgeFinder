a5accc6da0796403664136a3a21a3b0d66749610
matiasv93 <matiasvazquez93@gmail.com>
2023-03-17 23:42:46 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import Home from "./screens/Home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
