0ce1ffb139d046410d0d2cfdf0897b428bbcae2b
matiasv93 <matiasvazquez93@gmail.com>
2023-03-18 22:51:53 -0300

    Add logic and formatters for fetching ingredient and convert them to options for autocomplete

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
   Button,
   Flex,
@@ -7,11 +8,13 @@ import {
   List,
   ListItem,
 } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
-import Header from "../../components/Header";
+
 import { openai } from "../../services/openai/config";
 
-const formatRecipes = (data: any) => {
+import Header from "../../components/Header";
+import Autocomplete, { Option } from "../../components/Autocomplete";
+
+const formatDataFromAPI = (data: any) => {
   if (!data) return null;
 
   const text = data?.choices?.[0]?.text.replace(/^./, "") || "";
@@ -20,46 +23,84 @@ const formatRecipes = (data: any) => {
   return parsedObject;
 };
 
+const formatIngredientOptions = (ingredients: string[]) => {
+  const uniqueIngredients = ingredients.filter((x) => x);
+  return uniqueIngredients.map((ingredient) => ({
+    value: ingredient,
+    label: ingredient,
+  }));
+};
+
 const Home: React.FC = () => {
-  const [chatGPTRes, setChatGPTRes] = useState<any>(null);
+  const [receipes, setReceipes] = useState<any>(null);
+  const [receipesLoading, setReceipesLoading] = useState(false);
+  const [ingredientsLoading, setIngredientsLoading] = useState(false);
+  const [ingredients, setIngredients] = useState<Option[]>([]);
+  const [result, setResult] = useState<Option[]>([]);
 
   const handleFetchIngredients = async () => {
-    // const res = await openai.createCompletion({
-    //   model: "text-davinci-003",
-    //   prompt: `Give me a list of the 400 most used cocine ingredients in Argentina in JSON format`,
-    //   max_tokens: 3000,
-    //   top_p: 1,
-    //   temperature: 0.7,
-    // });
-    // const text = res?.data?.choices?.[0]?.text || "";
-    // console.log("ing: ", JSON.parse(text));
-    // setChatGPTRes(res.data.choices[0].text);
+    setIngredientsLoading(true);
+
+    try {
+      const res = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Give me an array of the 100 most used cocine ingredients in Argentina in JSON format`,
+        max_tokens: 3300,
+        top_p: 1,
+        temperature: 0.7,
+      });
+
+      setIngredientsLoading(false);
+
+      const formattedIngredients = formatDataFromAPI(res?.data);
+      const formatOptions = formatIngredientOptions(formattedIngredients);
+      setIngredients(formatOptions);
+    } catch (e) {
+      setIngredientsLoading(false);
+      console.log("error fetching ingredients: ", e);
+    }
   };
 
-  const handleFetchReceipe = async () => {
-    const res = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `Give me an array of 2 recipes in JSON format only with the following ingredients:
-      - 2 Eggs
-      - 1 carrot
-      - cheese
-      - cream
-      
-      I need the properties Name, Ingredients and Instructions in JSON format for each receipe`,
-      max_tokens: 1100,
-      top_p: 1,
-      temperature: 0.7,
-    });
+  const handleFetchReceipes = async () => {
+    setReceipesLoading(true);
 
-    const receipesData = formatRecipes(res?.data);
+    try {
+      const res = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Give me an array of 2 recipes in JSON format only with the following ingredients:
+        - 2 Eggs
+        - 1 carrot
+        - cheese
+        - cream
+        
+        I need the properties Name, Ingredients and Instructions in JSON format for each receipe`,
+        max_tokens: 1100,
+        top_p: 1,
+        temperature: 0.7,
+      });
 
-    setChatGPTRes(receipesData);
+      setReceipesLoading(false);
+
+      const receipesData = formatDataFromAPI(res?.data);
+      setReceipes(receipesData);
+    } catch (e) {
+      setReceipesLoading(false);
+      console.log("error fetching receipes: ", e);
+    }
   };
 
   useEffect(() => {
     handleFetchIngredients();
   }, []);
-  console.log("recipes: ", chatGPTRes);
+  console.log("receipes: ", receipes);
+  console.log("receipesLoading: ", receipesLoading);
+  console.log("ingredientsLoading: ", ingredientsLoading);
+  console.log("ingredients: ", ingredients);
+  console.log("result: ", result);
+
+  const disableFetchReceipesButton =
+    receipesLoading || ingredientsLoading || !ingredients.length;
+  console.log("disableFetchReceipesButton: ", disableFetchReceipesButton);
 
   return (
     <>
@@ -67,12 +108,33 @@ const Home: React.FC = () => {
         <Header />
         <Flex py={4} justifyContent="center">
           <Box maxW="1200px" w="full" flex={1} py={10}>
-            <Button onClick={handleFetchReceipe}>Fetch receipe</Button>
-            {chatGPTRes ? (
+            <Box py={10}>
+              {ingredients.length ? (
+                <Autocomplete
+                  result={result}
+                  setResult={setResult}
+                  options={ingredients}
+                />
+              ) : ingredientsLoading ? (
+                <Text>Loading ingredients...</Text>
+              ) : (
+                <Button onClick={handleFetchIngredients}>
+                  Fetch ingredients
+                </Button>
+              )}
+            </Box>
+            <Button
+              onClick={handleFetchReceipes}
+              isDisabled={disableFetchReceipesButton}
+              isLoading={receipesLoading}
+            >
+              Fetch receipes
+            </Button>
+            {receipes && !receipesLoading ? (
               <Box mt={10}>
                 <Heading>List:</Heading>
                 <Box>
-                  {chatGPTRes?.map((rec: any) => (
+                  {receipes?.map((rec: any) => (
                     <Box
                       mt={10}
                       bgColor="#e8cff980"
@@ -105,6 +167,8 @@ const Home: React.FC = () => {
                   ))}
                 </Box>
               </Box>
+            ) : receipesLoading ? (
+              <Text>Loading receipes...</Text>
             ) : null}
           </Box>
         </Flex>
